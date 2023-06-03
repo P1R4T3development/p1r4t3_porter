31df8e1645490861f863299177df24ec02322bdb
P1R4T3_Developpement <124460873+P1R4T3development@users.noreply.github.com>
2023-06-03 05:07:59 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+--------------------------------------------------------
+--                                                    --
+--               credits: x_P1R4T3_x                  --
+--                                                    --
+--------------------------------------------------------
+
+fx_version 'cerulean'
+game 'gta5'
+description 'P1R4T3_CORE'
+author 'P1R4T3'
+
+client_scripts "client/*.lua"
+
+server_script 'server/*.lua'
